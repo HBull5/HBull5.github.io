6493c2bf0fceb4cccc928caabe3dcf3ae88cfba5
hbull5 <harcourtbullv@gmail.com>
2020-05-12 22:48:11 -0400

    portfolio update --incomplete

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,38 @@
-var prevScrollpos = window.pageYOffset;
+let navbar = document.querySelector('#navbar');
+let toggle = document.querySelector('.toggle');
+let body = document.querySelector('body');
+let overlay = document.querySelector('.overlay');
+let prevScrollpos = window.pageYOffset;
+let bg = '#fff';
+let text = '#333';
+let filter = 'rgba(255, 255, 255, 0.7)';
+
 window.onscroll = function() {
-    var currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos) {
-        document.getElementById("navbar").style.top = "0";
+    let currentScrollPos = window.pageYOffset;
+    if(currentScrollPos === 0) {
+        navbar.style.background = 'none';
+    }
+    else if (prevScrollpos > currentScrollPos) {
+        navbar.style.top = "0";
+        navbar.style.background = bg;
     } else {
-        document.getElementById("navbar").style.top = "-68px";
+        navbar.style.top = "-68px";
+        navbar.style.background = bg;
     }
     prevScrollpos = currentScrollPos;
-    console.log(window.pageYOffset);
 }
+
+toggle.addEventListener('click', () => {
+    if(bg === '#fff') {
+        bg = '#333';
+        text = '#fff';
+        filter = 'rgba(0, 0, 0, 0.7)';
+    } else {
+        bg = '#fff';
+        text = '#333';
+        filter = 'rgba(255, 255, 255, 0.7)';
+    }
+    body.style.background = bg;
+    body.style.color = text;
+    overlay.style.background = filter;
+})
