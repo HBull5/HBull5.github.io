71281485858126106529257ce9d2e5206a2e55fb
hbull5 <harcourtbullv@gmail.com>
2020-05-15 15:36:41 -0400

    navbar 90% just a few hang ups with text and background-color

--- scripts/dark-mode-nav.js
+++ scripts/dark-mode-nav.js
@@ -1,11 +1,17 @@
 const navbars = document.querySelectorAll('.navbar');
-const toggle = document.querySelector('.toggle');
+const desktopNav = document.querySelector('#desktop-nav');
+const mobileNav = document.querySelector('#mobile-nav');
+const toggles = document.querySelectorAll('.toggle');
 const body = document.querySelector('body');
 const overlay = document.querySelector('.overlay');
 const darkOverlays = document.querySelectorAll('.overlay-dark');
 const buttons = document.querySelectorAll('.btn');
 const homeBtn = document.querySelector('#home-btn');
 const logos = document.querySelectorAll('.logo img');
+const mobileLogo = document.querySelector('#mobile-logo');
+const menuItems = document.querySelector('#menu-items');
+const open = document.querySelector('.open');
+const close = document.querySelector('.close');
 let prevScrollpos = window.pageYOffset;
 let bg = '#fff';
 let text = '#333';
@@ -17,7 +23,7 @@ window.onscroll = function() {
     let currentScrollPos = window.pageYOffset;
     for(navbar of navbars) {
         if(currentScrollPos === 0) {
-            navbar.style.background = 'none';
+            desktopNav.style.background = 'none';
         }
         else if (prevScrollpos > currentScrollPos) {
             navbar.style.top = "0";
@@ -28,7 +34,7 @@ window.onscroll = function() {
         }
     }
     prevScrollpos = currentScrollPos;
-}
+};
 
 const darkMode = () => {
     let currentScrollPos = window.pageYOffset;
@@ -38,18 +44,22 @@ const darkMode = () => {
         filter = 'rgba(0, 0, 0, 0.7)';
         darkFilter = 'rgba(100, 100, 100, 0.6)';
         logoColor = 'white';
-        toggle.innerHTML = '<i class="fas fa-sun"></i>';
+        for(toggle of toggles) {
+            toggle.innerHTML = '<i class="fas fa-sun"></i>';
+        }
     } else {
         bg = '#fff';
         text = '#333';
         filter = 'rgba(255, 255, 255, 0.6)';
         darkFilter = 'rgba(0, 0, 0, 0.7)';
         logoColor = 'black';
-        toggle.innerHTML = '<i class="fas fa-moon"></i>';
+        for(toggle of toggles) {
+            toggle.innerHTML = '<i class="fas fa-moon"></i>';
+        }
     }
     for(navbar of navbars) {
         if(currentScrollPos === 0) {
-            navbar.style.background = 'none';
+            desktopNav.style.background = 'none';
         } else {
             navbar.style.background = bg;
         }
@@ -67,20 +77,38 @@ const darkMode = () => {
     for(darkOverlay of darkOverlays) {
         darkOverlay.style.background = darkFilter;
     }
-}
+};
 
-toggle.addEventListener('click', () => {
-    darkMode();
-})
+for(toggle of toggles) {
+    toggle.addEventListener('click', () => {
+        darkMode();
+    })
+};
 
 homeBtn.addEventListener('mouseover', () => {
     for(logo of logos) {
         logo.setAttribute('src', 'img/green-logo.png');
     }
-})
+});
 
 homeBtn.addEventListener('mouseout', () => {
     for(logo of logos) {
         logo.setAttribute('src', `img/${logoColor}-logo.png`);
     }
+});
+
+open.addEventListener('click', () => {
+    body.style.overflow = 'hidden';
+    mobileNav.style.height = '100vh';
+    mobileLogo.style.display = 'none';
+    open.style.display = 'none';
+    menuItems.style.display = 'flex';
+})
+
+close.addEventListener('click', () => {
+    body.style.overflow = 'scroll';
+    mobileNav.style.height = '70px';
+    mobileLogo.style.display = 'block';
+    open.style.display = 'block';
+    menuItems.style.display = 'none';
 })
